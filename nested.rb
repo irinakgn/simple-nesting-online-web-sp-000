f341298d48a1dc76bf042eac2553ababde141638
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-23 01:32:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,10 +62,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash =
  		{
@@ -80,10 +77,14 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+				:yukihiro_matsumoto => {
+				  :known_for => "Ruby",
+				   :languages => ["LISP", "C"]
+				}
      }
 
-		 
+
 end
 
 def changing_alan
